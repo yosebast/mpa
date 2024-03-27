11ed605c68c008575a0c0e69ebdcdfe72a750035
yosebast <ik_sbtian@yahoo.es>
2024-03-27 19:23:37 +0100

    first commit

---
+++ src/main/java/com/example/mpa/MpabffApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpabffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpabffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mpa/MpabffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpabffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
